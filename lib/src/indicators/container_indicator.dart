496cacd589414d9e9b2cc222cb50ee800d6354fc
Sandro Lovnički <sandro@scipack.xyz>
2020-11-03 02:24:03 +0100

    {container_indicator}: [add] BorderRadius instead of double, borderWidth, borderColor

--- lib/src/indicators/container_indicator.dart
+++ lib/src/indicators/container_indicator.dart
@@ -3,49 +3,74 @@ import 'package:flutter/material.dart';
 class ContainerIndicator extends Decoration {
   final double width;
   final double height;
-  final double radius;
+  final BorderRadius radius;
   final Color color;
+  final double borderWidth;
+  final Color borderColor;
 
   const ContainerIndicator({
     this.width: 8.0,
     this.height: 8.0,
-    this.radius: 4.0,
+    this.radius: BorderRadius.zero,
     this.color: const Color(0xff000000),
+    this.borderWidth: 0.0,
+    this.borderColor: const Color(0xff000000),
   });
 
   @override
   _ContainerPainter createBoxPainter([void Function() onChanged]) {
     return _ContainerPainter(
       onChanged,
-      decoration: this,
+      indicator: this,
     );
   }
 }
 
 class _ContainerPainter extends BoxPainter {
-  final ContainerIndicator decoration;
+  final ContainerIndicator indicator;
 
   _ContainerPainter(
     void Function() onChanged, {
-    this.decoration: const ContainerIndicator(),
+    this.indicator: const ContainerIndicator(),
   }) : super(onChanged);
 
   @override
   void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
-    final RRect indicatorRRect = RRect.fromRectAndRadius(
+    if (this.indicator.borderWidth > 0.0) {
+      final RRect borderRRect = _buildRRect(
+        offset,
+        configuration,
+        addOnWidth: this.indicator.borderWidth,
+      );
+      final Paint paint = Paint()
+        ..color = this.indicator.borderColor
+        ..style = PaintingStyle.fill;
+      canvas.drawRRect(borderRRect, paint);
+    }
+
+    final RRect indicatorRRect = _buildRRect(offset, configuration);
+    final Paint paint = Paint()
+      ..color = this.indicator.color
+      ..style = PaintingStyle.fill;
+    canvas.drawRRect(indicatorRRect, paint);
+  }
+
+  RRect _buildRRect(
+    Offset offset,
+    ImageConfiguration configuration, {
+    double addOnWidth: 0.0,
+  }) {
+    return RRect.fromRectAndCorners(
       Rect.fromCenter(
         center: Offset(offset.dx + configuration.size.width / 2,
             configuration.size.height / 2),
-        width: this.decoration.width,
-        height: this.decoration.height,
+        width: this.indicator.width + 2 * addOnWidth,
+        height: this.indicator.height + 2 * addOnWidth,
       ),
-      Radius.circular(this.decoration.radius),
+      topLeft: this.indicator.radius.topLeft,
+      topRight: this.indicator.radius.topRight,
+      bottomRight: this.indicator.radius.bottomRight,
+      bottomLeft: this.indicator.radius.bottomLeft,
     );
-
-    final Paint paint = Paint()
-      ..color = this.decoration.color
-      ..style = PaintingStyle.fill;
-
-    canvas.drawRRect(indicatorRRect, paint);
   }
 }
