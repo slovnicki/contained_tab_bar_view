d0a2c67ffc7dbe07d858c9f39fd15aff20fbbb3c
Sandro Lovnički <sandro@scipack.xyz>
2020-11-08 08:19:58 +0100

    [add] container_tab_indicator package and expose it

--- lib/contained_tab_bar_view.dart
+++ lib/contained_tab_bar_view.dart
@@ -1,5 +1,7 @@
 library contained_tab_bar_view;
 
+export 'package:container_tab_indicator/container_tab_indicator.dart';
+
 export './src/enums.dart';
 export './src/indicators/indicators.dart';
 export './src/tab_bar_properties.dart';
